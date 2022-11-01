0475f4ef4e6098e00ac0b2c288787e9adec5d598
Anton Detev <det-anton@yandex.ru>
2022-11-01 21:34:21 +0400

    feat: добавлена интеграция TypeScript, React, tailwind

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,14 @@
 import { defineConfig } from 'astro/config';
+import react from '@astrojs/react';
+import tailwind from '@astrojs/tailwind';
 
 export default defineConfig({
     root: './client',
     srcDir: './client/src',
     publicDir: './client/public',
-    outDir: './client/dist'
+    outDir: './client/dist',
+    integrations: [
+        react(),
+        tailwind()
+    ]
 });
---
+++ client/src/components/Button.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const Button = () => (
+    <button>Click me</button>
+);
+
+export default Button;
